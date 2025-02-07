2beaf8142f828e628d777e6eebb085e79a14192f
Germano Rizzo <oss@germanorizzo.it>
2025-02-07 15:06:21 +0100

    Relicense to Apache v2

--- fileway_ul.py
+++ fileway_ul.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python3
-#
-# Copyright (C) 2024- Germano Rizzo
-#
-# This file is part of fileway.
-#
-# fileway is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# fileway is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with fileway.  If not, see <http://www.gnu.org/licenses/>.
-#
+#  Copyright 2024 @proofrock
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+ 
 import os
 
 import time
@@ -103,7 +97,7 @@ def upload_file(filepath):
 if __name__ == "__main__":
     import sys
 
-    print("== fileway v0.3.2 ==")
+    print("== fileway v0.0.0 ==")
     
     if len(sys.argv) < 2:
         print("Usage: python fileway_ul.py <file_path>")
